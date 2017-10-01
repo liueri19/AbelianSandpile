3a48482d271d1e15d1ec6f0756e635ffb930477d
liueri19 <liueri19@7hills.org>
2017-10-01 07:05:18 -0400

    complete basics

---
+++ src/org/_7hills/liueri19/Cell.java
@@ -0,0 +1,72 @@
+package org._7hills.liueri19;
+
+/**
+ * Each cell hold a certain amount of sand grains.
+ */
+public class Cell {
+	private final int x, y;
+	private final Ground ground;
+	private int sandGrains = 1;
+	
+	public Cell(int x, int y, Ground ground) {
+		this.x = x;
+		this.y = y;
+		this.ground = ground;
+	}
+
+	/**
+	 * Add one grain of sand to this cell.
+	 */
+	public void addSand() {
+		sandGrains++;
+	}
+
+	/**
+	 * Spill if this cell exceeds the spill threshold defined by the ground it is on.
+	 * @Return	true if this cell did spill, false otherwise
+	 */
+	public boolean checkSpill() {
+		boolean doSpill = sandGrains > ground.SPILL_THRESHOLD;
+		if (doSpill)
+			spill();
+		return doSpill;
+	}
+
+	/**
+	 * Reduce the sand grains on this cell by 4, spilling them to 4 adjacent cells.
+	 */
+	public void spill() {
+		ground.addSand(x - 1, y - 1);
+		ground.addSand(x - 1, y + 1);
+		ground.addSand(x + 1, y - 1);
+		ground.addSand(x + 1, y + 1);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+//	//this specific way of comparing is purposed to make output easier to implement.
+//	@Override
+//	public int compareTo(Cell c) {
+//		if (this.y > c.y)
+//			return -1;
+//		if (this.y < c.y)
+//			return 1;
+//		else {
+//			if (this.x < c.x)
+//				return -1;
+//			if (this.x > c.x)
+//				return 1;
+//		}
+//		return 0;
+//	}
+	
+//	public boolean equals(Cell c) {
+//		return this.x == c.x && this.y == c.y;
+//	}
+}
--- src/org/_7hills/liueri19/Ground.java
+++ src/org/_7hills/liueri19/Ground.java
@@ -1,42 +1,87 @@
 package org._7hills.liueri19;
 
-import java.awt.Color;
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
- * The plane that will hold the sand.
+ * The ground to place the sand pile on.
  */
-public class Ground {
+public class Ground extends JPanel {
 	/**
 	 * Sand grains would spill once exceeding this threshold, default to 4.
 	 */
 	public final int SPILL_THRESHOLD;
-	private int totalGrains = 0;
-	private List<Coordinate> coordinates = new ArrayList<>();
-	private Coordinate origin = new Coordinate(0, 0, this);
+	private int totalGrains = 1000;
+	private Cell[][] cells;
+	private int offset;
+	private Cell origin = new Cell(0, 0, this);
+	private ExecutorService executor = Executors.newSingleThreadExecutor();
 	
 	public Ground(int spillThreshold) {
-		this.SPILL_THRESHOLD = spillThreshold;
-		coordinates.add(origin);
+		SPILL_THRESHOLD = spillThreshold;
+		int sideLength = (int) Math.sqrt(totalGrains / Math.PI) * 2;
+		cells = new Cell[sideLength][sideLength];	//init a 2d array guaranteed to hold all sand
+		offset = sideLength / 2;
+		addSand(0, 0);
 	}
 	
 	public Ground() {
-		this(3);
+		this(4);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		//setup the window
 		Ground ground = new Ground();
+		JFrame frame = new JFrame("Sand Pile");
+		ground.setPreferredSize(new Dimension(1000, 1000));
+		ground.setBackground(Color.WHITE);
+		frame.add(ground);
+		frame.pack();
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		//thread for repaint
+		ground.executor.submit((Runnable) ground::repaint);	//the same thing as new Runnable calling repaint
+
+		for (int grain = 0; grain < ground.totalGrains; grain++) {
+			//add a sand, update, wait
+			ground.origin.addSand();
+			ground.updateSandPile();
+			Thread.sleep(250);
+		}
 	}
-	
-	//helper method for drawing image
-	static Color getMatchingColor(int grains) {
-		if (grains == 0)
-			return Color.BLACK;
-		if (grains == 1)
-			return new Color(85, 85, 85);
-		if (grains == 2)
-			return new Color(170, 170, 170);
-		return Color.WHITE;
+
+	/**
+	 * Add one grain of sand to the specified cell.
+	 * @param x	the x location of the cell
+	 * @param y	the y location of the cell
+	 */
+	public void addSand(int x, int y) {
+		Cell cell = cells[x + offset][y + offset];
+		if (cell == null)
+			cells[x + offset][y + offset] = new Cell(x, y, this);
+		else
+			cell.addSand();
+	}
+
+	public void updateSandPile() {
+		boolean completed = false;
+		while (!completed) {
+			completed = true;
+			for (Cell[] row : cells) {
+				for (Cell cell : row) {
+					if (cell == null)
+						continue;
+					if (cell.checkSpill())
+						completed = false;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics graphics) {
+
 	}
 }
